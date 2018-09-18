d7ac8c5a4e155762ba9ea7159cc49ab6b7092058
zoso007 <gregsjanzen@gmail.com>
2018-09-18 16:45:11 -0600

    Handle stopping and starting instances when snapshotting. Handle errors from AWS when attempting to start or stop an instance in the wrong state

--- shotty/shotty.py
+++ shotty/shotty.py
@@ -1,4 +1,5 @@
 import boto3
+import botocore
 import click
 
 session = boto3.Session(profile_name='shotty')
@@ -85,9 +86,18 @@ def create_snapshots(project):
 	instances = filter_instances(project)
 
 	for i in instances:
+		print("Stopping {0}...".format(i.id))
+		i.stop()
+		i.wait_until_stopped()
 		for v in i.volumes.all():
-			print("Creating snapthot of {0}".format(v.id))
+			print("   Creating snapthot of {0}".format(v.id))
 			v.create_snapshot(Description="Created by SnapshotAlyzer 30000")
+		print("Starting {0}...".format(i.id))
+		i.start()
+		i.wait_until_running()
+
+	print("Snapshot job is complete!")
+
 	return
 
 
@@ -124,7 +134,11 @@ def stop_instances(project):
 	
 	for i in instances:
 		print("Stopping {0}...".format(i.id))
-		i.stop()
+		try:
+			i.stop()
+		except botocore.exceptions.ClientError as e:
+			print(" Could not stop {0}. ".format(i.id) + str(e))
+			continue
 	return
 
 
@@ -139,7 +153,11 @@ def stop_instances(project):
 	
 	for i in instances:
 		print("Starting {0}...".format(i.id))
-		i.start()
+		try:
+			i.start()
+		except botocore.exceptions.ClientError as e:
+			print(" Could not start {0}. ".format(i.id) + str(e))
+			continue
 	return
 
 
